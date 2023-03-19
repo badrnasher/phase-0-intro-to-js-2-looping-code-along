7f125593f664a9944db2d3cae09ff0758e6091c8
Badr Nasher <badrnasher@gmail.com>
2023-03-19 18:03:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray;
+}
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+countDown(10);
